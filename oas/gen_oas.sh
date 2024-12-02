8a49c04b9f03e30270059464bb38602659c41fb0
Gerard Juijn <gerard@affinii.nl>
2024-12-02 16:54:44 +0100

    add AuthZEN OpenAPI spec

--- oas/gen_oas.sh
+++ oas/gen_oas.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-# clone FSC repo
+# FSC
+# clone repo
 cd fsc
 git clone --depth 1 https://gitlab.com/commonground/nlx/fsc-nlx.git
 
 cp fsc-nlx/outway/authorization-interface.yaml auth/oas-auth.yaml
 
+# generate
 cd auth
 oapi-codegen -config cfg-auth.yaml oas-auth.yaml
 cd ..
 
-# remove FSC repo
+# remove repo
 rm -Rf fsc-nlx
+cd ..
+
+# AuthZEN
+cd authzen
+oapi-codegen -config cfg-authzen.yaml oas-authzen.yaml
 cd ..
