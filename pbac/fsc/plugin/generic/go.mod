2e9789573a8f8ff887aa32346d77447f6a1f8f53
Gerard Juijn <gerard@affinii.nl>
2024-12-21 11:47:30 +0000

    extend unit tests

--- pbac/fsc/plugin/generic/go.mod
+++ pbac/fsc/plugin/generic/go.mod
@@ -9,10 +9,10 @@ require (
 	github.com/gofiber/fiber/v2 v2.52.5
 	github.com/google/uuid v1.6.0
 	github.com/stretchr/testify v1.10.0
-	gitlab.com/digilab.overheid.nl/ecosystem/ftv/ftv-implementatie/oas v0.0.0-20241220094324-2998b074e436
-	gitlab.com/digilab.overheid.nl/ecosystem/ftv/ftv-implementatie/pbac/components v0.0.0-20241220094324-2998b074e436
-	gitlab.com/digilab.overheid.nl/ecosystem/ftv/ftv-implementatie/pbac/models v0.0.0-20241220094324-2998b074e436
-	gitlab.com/digilab.overheid.nl/ecosystem/ftv/ftv-implementatie/utilities v0.0.0-20241220094324-2998b074e436
+	gitlab.com/digilab.overheid.nl/ecosystem/ftv/ftv-implementatie/oas v0.0.0-20241220185029-f086f33b2e2f
+	gitlab.com/digilab.overheid.nl/ecosystem/ftv/ftv-implementatie/pbac/components v0.0.0-20241220185029-f086f33b2e2f
+	gitlab.com/digilab.overheid.nl/ecosystem/ftv/ftv-implementatie/pbac/models v0.0.0-20241220185029-f086f33b2e2f
+	gitlab.com/digilab.overheid.nl/ecosystem/ftv/ftv-implementatie/utilities v0.0.0-20241220185029-f086f33b2e2f
 	gitlab.com/gjuyn/go-config v1.0.1
 	go.opentelemetry.io/otel v1.32.0
 	go.opentelemetry.io/otel/trace v1.32.0
--- pbac/fsc/plugin/generic/handlers/policies_test.go
+++ pbac/fsc/plugin/generic/handlers/policies_test.go
@@ -1,11 +1,13 @@
 package handlers
 
 import (
+	"bytes"
 	"io"
 	"log/slog"
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/goccy/go-json"
 	"github.com/gofiber/fiber/v2"
@@ -54,7 +56,7 @@ func TestPoliciesHandler_GetPolicies(t *testing.T) {
 		srv := fiber.New()
 		srv.Get("/v1/policies", ph.GetPolicies)
 
-		req := httptest.NewRequest("GET", "/v1/policies", nil)
+		req := httptest.NewRequest(fiber.MethodGet, "/v1/policies", nil)
 		resp, err2 := srv.Test(req, 1)
 
 		require.NoError(t, err2)
@@ -124,7 +126,320 @@ func TestPoliciesHandler_GetPolicy(t *testing.T) {
 			srv := fiber.New()
 			srv.Get("/v1/policy/:id", ph.GetPolicy)
 
-			req := httptest.NewRequest("GET", "/v1/policy/"+tc.id, nil)
+			req := httptest.NewRequest(fiber.MethodGet, "/v1/policy/"+tc.id, nil)
+			resp, err2 := srv.Test(req, 1)
+
+			require.NoError(t, err2)
+			require.NotNil(t, resp)
+			defer resp.Body.Close()
+
+			assert.Equal(t, tc.wantStatus, resp.StatusCode)
+
+			if tc.wantStatus == fiber.StatusOK {
+				b, err3 := io.ReadAll(resp.Body)
+				require.NoError(t, err3)
+				require.NotNil(t, b)
+
+				var pol policies.Policy
+				err = json.Unmarshal(b, &pol)
+				require.NoError(t, err)
+				assert.NotNil(t, pol)
+			}
+		})
+	}
+}
+
+func TestPoliciesHandler_PutPolicy(t *testing.T) {
+	badURL := `{
+ "source": "s1",
+ "target": "t1",
+ "rvvaID": "id1",
+ "url": "http://localhost:10000/policy/xyzqqq"
+}`
+
+	goodURL := `{
+ "source": "s1",
+ "target": "t1",
+ "rvvaID": "id1",
+ "url": "https://gitlab.com/digilab.overheid.nl/ecosystem/ftv/ftv-implementatie/-/blob/f086f33b2e2fc51e494e0a22bc7bbcc045847250/testdata/policies/cedar/brp/subsidies.cedar"
+}`
+
+	testCases := []struct {
+		name       string
+		id         string
+		body       io.Reader
+		timeout    time.Duration
+		wantStatus int
+	}{
+		{
+			name:       "no ID",
+			body:       bytes.NewBufferString(""),
+			timeout:    time.Millisecond,
+			wantStatus: fiber.StatusNotFound,
+		},
+		{
+			name:       "very long ID",
+			id:         strings.Repeat("x", 501),
+			body:       bytes.NewBufferString(""),
+			timeout:    time.Millisecond,
+			wantStatus: fiber.StatusBadRequest,
+		},
+		{
+			name:       "no body",
+			id:         "xyz",
+			timeout:    time.Millisecond,
+			wantStatus: fiber.StatusBadRequest,
+		},
+		{
+			name:       "bad body",
+			id:         "xyz",
+			body:       bytes.NewBufferString("my policy 1.0"),
+			timeout:    time.Millisecond,
+			wantStatus: fiber.StatusBadRequest,
+		},
+		{
+			name:       "bad url",
+			id:         "xyz",
+			body:       bytes.NewBufferString(badURL),
+			timeout:    5 * time.Second,
+			wantStatus: fiber.StatusBadRequest,
+		},
+		{
+			name:       "duplicate id",
+			id:         "subsidies.cedar",
+			body:       bytes.NewBufferString(goodURL),
+			timeout:    5 * time.Second,
+			wantStatus: fiber.StatusConflict,
+		},
+		{
+			name:       "all good",
+			id:         "xyz",
+			body:       bytes.NewBufferString(goodURL),
+			timeout:    5 * time.Second,
+			wantStatus: fiber.StatusOK,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			cfg := &cfg2.Config{
+				PolicyLanguage:     "cedar",
+				PolicyStore:        "../../../../../testdata/policies/cedar",
+				PolicyStoreRecurse: true,
+				PipStore:           "../../../../../testdata/pip",
+				PipStoreRecurse:    true,
+			}
+
+			h := slog2.NewDummyHandler(slog.LevelInfo)
+			logger := slog.New(h)
+
+			c, err := newController(cfg, logger, nil)
+			require.NoError(t, err)
+			require.NotNil(t, c)
+
+			ph := NewPoliciesHandler(cfg, logger, c)
+			require.NotNil(t, ph)
+
+			srv := fiber.New()
+			srv.Put("/v1/policy/:id", ph.PutPolicy)
+
+			req := httptest.NewRequest(fiber.MethodPut, "/v1/policy/"+tc.id, tc.body)
+			req.Header.Add(fiber.HeaderContentType, fiber.MIMEApplicationJSON)
+
+			resp, err2 := srv.Test(req, int(tc.timeout/time.Millisecond))
+
+			require.NoError(t, err2)
+			require.NotNil(t, resp)
+			defer resp.Body.Close()
+
+			assert.Equal(t, tc.wantStatus, resp.StatusCode)
+
+			if tc.wantStatus == fiber.StatusOK {
+				b, err3 := io.ReadAll(resp.Body)
+				require.NoError(t, err3)
+				require.NotNil(t, b)
+
+				var pol policies.Policy
+				err = json.Unmarshal(b, &pol)
+				require.NoError(t, err)
+				assert.NotNil(t, pol)
+			}
+		})
+	}
+}
+
+func TestPoliciesHandler_PostPolicy(t *testing.T) {
+	badURL := `{
+ "source": "s1",
+ "target": "t1",
+ "rvvaID": "id1",
+ "url": "http://localhost:10000/policy/xyzqqq"
+}`
+
+	goodURL := `{
+ "source": "s1",
+ "target": "t1",
+ "rvvaID": "id1",
+ "url": "https://gitlab.com/digilab.overheid.nl/ecosystem/ftv/ftv-implementatie/-/blob/f086f33b2e2fc51e494e0a22bc7bbcc045847250/testdata/policies/cedar/brp/subsidies.cedar"
+}`
+
+	testCases := []struct {
+		name       string
+		id         string
+		body       io.Reader
+		timeout    time.Duration
+		wantStatus int
+	}{
+		{
+			name:       "no ID",
+			body:       bytes.NewBufferString(""),
+			timeout:    time.Millisecond,
+			wantStatus: fiber.StatusNotFound,
+		},
+		{
+			name:       "very long ID",
+			id:         strings.Repeat("x", 501),
+			body:       bytes.NewBufferString(""),
+			timeout:    time.Millisecond,
+			wantStatus: fiber.StatusBadRequest,
+		},
+		{
+			name:       "no body",
+			id:         "xyz",
+			timeout:    time.Millisecond,
+			wantStatus: fiber.StatusBadRequest,
+		},
+		{
+			name:       "bad body",
+			id:         "xyz",
+			body:       bytes.NewBufferString("my policy 1.0"),
+			timeout:    time.Millisecond,
+			wantStatus: fiber.StatusBadRequest,
+		},
+		{
+			name:       "bad url",
+			id:         "xyz",
+			body:       bytes.NewBufferString(badURL),
+			timeout:    5 * time.Second,
+			wantStatus: fiber.StatusBadRequest,
+		},
+		{
+			name:       "not found",
+			id:         "xyz",
+			body:       bytes.NewBufferString(goodURL),
+			timeout:    5 * time.Second,
+			wantStatus: fiber.StatusNotFound,
+		},
+		{
+			name:       "all good",
+			id:         "subsidies.cedar",
+			body:       bytes.NewBufferString(goodURL),
+			timeout:    5 * time.Second,
+			wantStatus: fiber.StatusOK,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			cfg := &cfg2.Config{
+				PolicyLanguage:     "cedar",
+				PolicyStore:        "../../../../../testdata/policies/cedar",
+				PolicyStoreRecurse: true,
+				PipStore:           "../../../../../testdata/pip",
+				PipStoreRecurse:    true,
+			}
+
+			h := slog2.NewDummyHandler(slog.LevelInfo)
+			logger := slog.New(h)
+
+			c, err := newController(cfg, logger, nil)
+			require.NoError(t, err)
+			require.NotNil(t, c)
+
+			ph := NewPoliciesHandler(cfg, logger, c)
+			require.NotNil(t, ph)
+
+			srv := fiber.New()
+			srv.Post("/v1/policy/:id", ph.PostPolicy)
+
+			req := httptest.NewRequest(fiber.MethodPost, "/v1/policy/"+tc.id, tc.body)
+			req.Header.Add(fiber.HeaderContentType, fiber.MIMEApplicationJSON)
+
+			resp, err2 := srv.Test(req, int(tc.timeout/time.Millisecond))
+
+			require.NoError(t, err2)
+			require.NotNil(t, resp)
+			defer resp.Body.Close()
+
+			assert.Equal(t, tc.wantStatus, resp.StatusCode)
+
+			if tc.wantStatus == fiber.StatusOK {
+				b, err3 := io.ReadAll(resp.Body)
+				require.NoError(t, err3)
+				require.NotNil(t, b)
+
+				var pol policies.Policy
+				err = json.Unmarshal(b, &pol)
+				require.NoError(t, err)
+				assert.NotNil(t, pol)
+			}
+		})
+	}
+}
+
+func TestPoliciesHandler_DeletePolicy(t *testing.T) {
+	testCases := []struct {
+		name       string
+		id         string
+		wantStatus int
+	}{
+		{
+			name:       "no ID",
+			wantStatus: fiber.StatusNotFound,
+		},
+		{
+			name:       "very long ID",
+			id:         strings.Repeat("x", 501),
+			wantStatus: fiber.StatusBadRequest,
+		},
+		{
+			name:       "not found",
+			id:         "xyz",
+			wantStatus: fiber.StatusNotFound,
+		},
+		{
+			name:       "all good",
+			id:         "subsidies.cedar",
+			wantStatus: fiber.StatusOK,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			cfg := &cfg2.Config{
+				PolicyLanguage:     "cedar",
+				PolicyStore:        "../../../../../testdata/policies/cedar",
+				PolicyStoreRecurse: true,
+				PipStore:           "../../../../../testdata/pip",
+				PipStoreRecurse:    true,
+			}
+
+			h := slog2.NewDummyHandler(slog.LevelInfo)
+			logger := slog.New(h)
+
+			c, err := newController(cfg, logger, nil)
+			require.NoError(t, err)
+			require.NotNil(t, c)
+
+			ph := NewPoliciesHandler(cfg, logger, c)
+			require.NotNil(t, ph)
+
+			srv := fiber.New()
+			srv.Delete("/v1/policy/:id", ph.DeletePolicy)
+
+			req := httptest.NewRequest(fiber.MethodDelete, "/v1/policy/"+tc.id, nil)
+			req.Header.Add(fiber.HeaderContentType, fiber.MIMEApplicationJSON)
+
 			resp, err2 := srv.Test(req, 1)
 
 			require.NoError(t, err2)
