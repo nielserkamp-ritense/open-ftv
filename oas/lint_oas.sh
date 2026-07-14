72d111d0b44c75152d71be329cf1f592eae406d4
ndequeker <niels@nd-software.be>
2026-07-14 09:55:15 +0200

    fix: pin ADR to v1.0.0 when linting our OAS
    ADR v2 contains braking changes. We should update our OAS files too when updating.

--- oas/lint_oas.sh
+++ oas/lint_oas.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-curl -s https://static.developer.overheid.nl/adr/ruleset.yaml > .spectral.yml
+# Linting currently passes for ADR v1.0.0. But since ADR has been updated to v2.0.0,
+# we need to update our OpenAPI specs to be compatible first.
+# https://gitdocumentatie.logius.nl/publicatie/api/adr/<version>/media/linter.yaml)
 
 spectral lint -r .spectral.yml liveness/openapi.yaml
 spectral lint -r .spectral.yml attributes/openapi.yaml
